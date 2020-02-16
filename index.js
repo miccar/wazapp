a96cec5d9a3bd3aa6a43c4bbd1e669584df0bab6
miccar <m>
2020-02-16 15:09:51 +0100

    update

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function createWindow() {
 	wscc.setMenuBarVisibility(false);
 
 	//set user agent of browser #avoid whatsapp error on chromium browser
-	wscc.webContents.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
+	wscc.webContents.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
 	wscc.loadURL("https://web.whatsapp.com");
 
 
